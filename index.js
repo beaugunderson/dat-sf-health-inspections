0597af2d799e45c5a6621f5bc1da7aea1756c81c
Beau Gunderson <beau@beaugunderson.com>
2014-02-11 23:20:31 -0800

    Fetch and insert the data

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 var Dat = require('dat');
+var request = require('request');
+var unzip = require('unzip');
 
 var PORT = process.env.PORT || 8080;
 
+var dat;
+
 function fetch() {
   console.log('Fetching...');
+  console.log(JSON.stringify({ 'started': new Date() }));
+
+  request.get('https://extxfer.sfdph.org/food/SFBusinesses.zip')
+    .pipe(unzip.Parse())
+    .on('entry', function (entry) {
+      if (entry.path === 'inspections.csv') {
+        var writeStream = dat.createWriteStream({
+          csv: true,
+          primary: ['business_id', 'date'],
+          hash: true
+        });
+
+        entry.pipe(writeStream);
+
+        writeStream.on('error', function (err) {
+          console.error('Error', err);
+        });
+
+        writeStream.on('close', function () {
+          console.log(JSON.stringify({ 'finished': new Date() }));
+        });
+      } else {
+        entry.autodrain();
+      }
+    });
 }
 
-var dat = new Dat('./data', function ready(err) {
+dat = new Dat('./data', function ready(err) {
   if (err) {
     throw err;
   }
@@ -14,6 +43,6 @@ var dat = new Dat('./data', function ready(err) {
   dat.serve({ port: PORT }, function () {
     console.log('Listening on port %d', PORT);
 
-    setInterval(fetch, 1000 * 60 * 60 * 24);
+    fetch();
   });
 });
